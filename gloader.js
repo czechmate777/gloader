03e5644952d89ba204111806d187cf6a4d1f46af
Adam Miller <adamtm777@gmail.com>
2017-02-22 11:58:16 -0800

    Update gloader.js

--- gloader.js
+++ gloader.js
@@ -1,3 +1,5 @@
+// Copyright (C) 2017  czechmate777
+// Inspired by jczimm: https://codepen.io/jczimm/pen/vEBpoL
 $(document).ready(function(){
   $('head').prepend('<style>.gloader{position:relative}.gloaderinner{position:absolute;top:50%;left:50%;width:100px;height:100px;transform:translate(-50px,-50px);pointer-events:none}.gloaderinner>.gcircular{animation:gload 1s linear infinite;height:100px;width:100px}.gloaderinner>.gcircular>.gpath{animation:gash 1.5s ease-in-out infinite;stroke:#000;stroke-width:3;stroke-dasharray:1,200;stroke-dashoffset:0;stroke-linecap:square}@keyframes gload{100%{transform:rotate(360deg)}}@keyframes gash{0%{stroke-dasharray:1,200;stroke-dashoffset:0}50%{stroke-dasharray:89,200;stroke-dashoffset:-35}100%{stroke-dasharray:89,200;stroke-dashoffset:-124}}</style>');
   $('.gloader').each(function(gi,gg){
