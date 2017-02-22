1177562b3147eceb418f9c32632512feccf81b95
Adam Miller <adamtm777@gmail.com>
2017-02-22 11:36:06 -0800

    Update gloader.js
    Now `data-gloader-` attributes with css values can be passed in.
    Ex: `data-gloader-stroke="#fff"`

--- gloader.js
+++ gloader.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
   $('head').prepend('<style>.gloader{position:relative}.gloaderinner{position:absolute;top:50%;left:50%;width:100px;height:100px;transform:translate(-50px,-50px);pointer-events:none}.gloaderinner>.gcircular{animation:gload 1s linear infinite;height:100px;width:100px}.gloaderinner>.gcircular>.gpath{animation:gash 1.5s ease-in-out infinite;stroke:#000;stroke-width:3;stroke-dasharray:1,200;stroke-dashoffset:0;stroke-linecap:square}@keyframes gload{100%{transform:rotate(360deg)}}@keyframes gash{0%{stroke-dasharray:1,200;stroke-dashoffset:0}50%{stroke-dasharray:89,200;stroke-dashoffset:-35}100%{stroke-dasharray:89,200;stroke-dashoffset:-124}}</style>');
-  $('.gloader').each(function(){
+  $('.gloader').each(function(gi,gg){
     $(this).append('<div class="gloaderinner"><svg class="gcircular"><circle class="gpath" cx="50" cy="50" r="20" fill="none"/></svg></div>');
+    $.each(this.attributes, function(ai,aa){
+      if (aa.name.substr(0,13) == "data-gloader-") {
+        $(gg).find(".gpath").css(aa.name.substr(13),aa.value);
+      }
+    });
   });
 });
